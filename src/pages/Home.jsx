4b6c819c2d9fdf28815e950f30fa525a751927aa
Shaurya Saria <sariashaurya09@gmail.com>
2026-06-24 21:19:56 +0530

    Updated homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,2 +1,17 @@
 import Hero from "../components/Hero";
-export default function Home() { return <Hero />; }
+import Skills from "../components/skills";
+import Projects from "../components/Projects";
+import Timeline from "../components/timeline";
+
+export default function Home() {
+    return (
+        <>
+            <Hero />
+            <div className="home-sections">
+                <Skills />
+                <Projects />
+                <Timeline />
+            </div>
+        </>
+    );
+}
