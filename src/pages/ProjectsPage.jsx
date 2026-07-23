bee175169b1cd7476700ee4588f924d8bbdc0d1e
Shaurya Saria <sariashaurya09@gmail.com>
2026-07-23 21:25:49 +0530

    refactor: finalize editorial projects experience

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,11 +1,5 @@
 import Projects from "../components/Projects";
+
 export default function ProjectsPage() {
-    return (
-        <div className="page-wrapper">
-            <div className="page-header">
-                <p className="page-breadcrumb">projects</p>
-            </div>
-            <Projects />
-        </div>
-    );
+    return <Projects />;
 }
