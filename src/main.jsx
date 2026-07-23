1c6e3c20a8423ead550bed5b74a69b8141f29b82
Shaurya Saria <sariashaurya09@gmail.com>
2026-07-23 20:01:22 +0530

    feat: enable global Lenis smooth scrolling

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ReactLenis } from "lenis/react";
+
 import App from "./App.jsx";
+import "lenis/dist/lenis.css";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <ReactLenis
+            root
+            options={{
+                autoRaf: true,
+                anchors: true,
+                lerp: 0.085,
+                smoothWheel: true,
+                wheelMultiplier: 0.9,
+                syncTouch: false,
+            }}
+        >
+            <App />
+        </ReactLenis>
     </React.StrictMode>
 );
